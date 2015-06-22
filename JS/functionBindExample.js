0a5d8a7a00f6285c15319d711a4890d37d649531
Kevin Berg <kevinmarkvi@gmail.com>
2015-06-22 15:56:05 -0700

    Add Scope problem and Uniqe Chars update

--- JS/functionBindExample.js
+++ JS/functionBindExample.js
@@ -13,5 +13,15 @@ var boundTest = obj.prop.getName.bind(obj.prop);
 
 console.log(obj.prop.getName());
 var test = obj.prop.getName;
-console.log(test());
-console.log(boundTest());
+console.log("GLOBAL: ", test());
+console.log("FUNCTION BIND: ", boundTest());
+
+console.log("CALL: ", test.call(obj.prop));
+
+console.log("Apply: ", test.call(obj.prop, []));
+
+//function.prototype.call(thisContext, argument) 
+
+//funtion.prototype.apply(thisContext, [array of arguments])
+
+//Great article: http://javascriptissexy.com/javascript-apply-call-and-bind-methods-are-essential-for-javascript-professionals/
