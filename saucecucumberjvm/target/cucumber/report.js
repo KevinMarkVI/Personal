42f66361f834b7926689004aa048955c3d01a3cc
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-10-06 10:10:50 -0700

    Add cucumberjvm framework

---
+++ saucecucumberjvm/target/cucumber/report.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri('com/yourcompany/saucecucumberjvm/guineapig.feature');
+formatter.feature({
+  "line": 1,
+  "name": "Sauce Labs Guinea Pig",
+  "description": "",
+  "id": "sauce-labs-guinea-pig",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "We want to click on the link and verify it takes us to a new page",
+  "description": "",
+  "id": "sauce-labs-guinea-pig;we-want-to-click-on-the-link-and-verify-it-takes-us-to-a-new-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on the Sauce Labs Guinea Pig test page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I click on the link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I should see a new page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GuineaPigSteps.user_is_on_guinea_pig_page()"
+});
+formatter.result({
+  "duration": 9083358751,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GuineaPigSteps.user_click_on_the_link()"
+});
+formatter.result({
+  "duration": 487948869,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GuineaPigSteps.new_page_displayed()"
+});
+formatter.result({
+  "duration": 150925713,
+  "status": "passed"
+});
+});
